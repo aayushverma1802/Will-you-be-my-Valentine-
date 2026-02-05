accc78d55cd69a5b2fcd7b5b0c6cbd4a4b297d2f
aayushverma1802 <aayush.verma2021@vitstudent.ac.in>
2026-02-05 18:40:36 +0530

    Remove handler - Vercel auto-detects Flask app

--- app.py
+++ app.py
@@ -12,8 +12,5 @@ def index():
 def serve_static(path):
     return send_from_directory('static', path)
 
-# Vercel serverless function handler
-handler = app
-
 if __name__ == '__main__':
     app.run(debug=True)
